845e9e08e696d1d020a305dcae6de8cca9d300e5
hwangjf <jeffreyhwang90@gmail.com>
2019-03-26 13:59:55 -0400

    added number to batches

--- db/migrate/20190326175050_add_activity_reference_to_groups.rb
+++ db/migrate/20190326175050_add_activity_reference_to_groups.rb
@@ -1,5 +1,5 @@
 class AddActivityReferenceToGroups < ActiveRecord::Migration[5.2]
   def change
-    add_reference :groups, :activity, foreign_key: true
+    add_column :groups, :number, :integer
   end
 end
