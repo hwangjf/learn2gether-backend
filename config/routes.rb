009ebfad7d3085fbb87dbe6db318aa56cffcce84
Charlie Russo <cparusso@gmail.com>
2019-04-02 17:21:13 -0400

    Add resources to everything.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :instructors
+      resources :batches
+      resources :activities
+      resources :groups
+      resources :instructor_batches
+      resources :locations
+      resources :student_batches
+      resources :student_groups
+      resources :students
+    end
+  end
 end
